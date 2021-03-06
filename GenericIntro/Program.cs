fae9f53d1847cf06779f9139592540c6a4589e57
Davut Aktaş <engineerdavut@gmail.com>
2021-03-06 21:19:41 +0300

    en guncel 21.19

---
+++ GenericIntro/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GenericIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MyList<string> mylist = new MyList<string>();
+            mylist.Add("davut");
+            mylist.Add("tarık");
+            mylist.Add("bakır");
+            Console.WriteLine(mylist.Length);
+            mylist.Add("hiroo");
+            Console.WriteLine(mylist.Length);
+
+            foreach (string isim in mylist.Items)
+            {
+                Console.WriteLine(isim);
+            }
+
+
+        }
+    }
+}
---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string[] isimler = new string[] { "ali", "mehmet" };
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[1]);
+            ///Console.WriteLine(isimler[2]); patlar. Listler collectionlar kullanılır.
+
+            List<string> isimler2 = new List<string> { "Ali", "MEHMET", "mahmut" };
+            isimler2.Add("Cihan");
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+        }
+    }
+}
