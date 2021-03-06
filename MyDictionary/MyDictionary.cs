308fea1a3931438fad793ee62b96514f43ae72a4
Davut Aktaş <engineerdavut@gmail.com>
2021-03-07 00:08:50 +0300

    00.08

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempKey = keys;
+            TValue[] tempValue = values;
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+                values[i] = tempValue[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+        }
+        public int Length { get { return keys.Length; } }
+
+        public TKey[] Items { get { return keys; } }
+        public TValue[] Items1 { get { return values; } }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MyDictionary<int ,string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(06,"Ankara");
+            myDictionary.Add(61, "Trabzon");
+            myDictionary.Add(34, "İstanbul");
+            myDictionary.Add(27, "GaziAntep");
+            Console.WriteLine(myDictionary.Length);
+            myDictionary.Add(44, "Malatya");
+            Console.WriteLine(myDictionary.Length);
+            for (int i= 0; i < myDictionary.Items.Length; i++)
+            {
+                Console.WriteLine(myDictionary.Items[i]+"      "+myDictionary.Items1[i]);
+            }
+        }
+    }
+}
